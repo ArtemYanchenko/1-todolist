752bc122d39c3bc2b91d2971ee424b037818d4b6
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-06-10 17:33:50 +0300

    add app-reducer in 15 lesson

---
+++ src/bll/app-reducer.ts
@@ -0,0 +1,14 @@
+
+const initialState = {
+    isLoading:true
+}
+
+export type AppActionType = {
+    isLoading:boolean
+}
+
+export const appReducer = (state = initialState,action:AppActionType) => {
+    switch () {
+
+    }
+}
