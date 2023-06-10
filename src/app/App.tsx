9d531ee9f55d47341da81e35dd821e86860c772a
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-06-10 17:29:47 +0300

    add 15 lesson

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {TodolistsList} from '../features/TodolistList/TodolistsList';
 import {Header} from '../features/Header/Header';
+import {LinearColor} from '../features/Header/PreLoader';
 
 
 export function App() {
     return (
         <div className="App">
             <Header/>
+            <LinearColor/>
             <TodolistsList/>
         </div>
     );
