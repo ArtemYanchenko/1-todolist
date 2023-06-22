e25293400c2169affa4b3e4675d91332b389419c
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-06-22 23:10:59 +0300

    final done in 4 lesson

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,10 +13,11 @@ export function App() {
             <Routes>
                 <Route path={'/'} element={<TodolistsList/>}/>
                 <Route path={'/login'} element={<Login/>}/>
-                <Route path={'/404'} element={<h1 style={{display:'flex',justifyContent:'center', alignItems:'center'}}>PAGE NOT FOUND</h1>}/>
+                <Route path={'/404'}
+                       element={<h1 style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>PAGE NOT
+                           FOUND:</h1>}/>
                 <Route path={'*'} element={<Navigate to={'404'}/>}/>
             </Routes>
-
         </div>
     );
 }
