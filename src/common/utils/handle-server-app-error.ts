eca0d0f426166a6f4c4d1074de9e4f4732afafef
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-07-08 23:39:54 +0300

    refactor handle-server-app-error in 19 lesson

--- src/common/utils/handle-server-app-error.ts
+++ src/common/utils/handle-server-app-error.ts
@@ -2,6 +2,13 @@ import { ResponseType } from "common/dal/api";
 import { Dispatch } from "redux";
 import { appActions } from "app/appReducer";
 
+/**
+ *
+ * @param data
+ * @param dispatch
+ * @param showError
+ */
+
 export const handleServerAppError = <D>(data: ResponseType<D>, dispatch: Dispatch, showError: boolean = true) => {
   if (showError) {
     dispatch(appActions.setError({ error: data.messages.length ? data.messages[0] : "Some error occurred" }));
--- src/common/utils/handle-server-network-error.ts
+++ src/common/utils/handle-server-network-error.ts
@@ -2,6 +2,12 @@ import { Dispatch } from "redux";
 import { appActions } from "app/appReducer";
 import axios, { AxiosError } from "axios";
 
+/**
+ *
+ * @param e
+ * @param dispatch
+ */
+
 export const handleServerNetworkError = (e: unknown, dispatch: Dispatch) => {
   const err = e as Error | AxiosError<{ error: string }>;
   if (axios.isAxiosError(err)) {
