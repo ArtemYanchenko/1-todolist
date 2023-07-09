6c96795d2ebb88c0597e31a41b01397e30abd55e
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-07-09 21:34:35 +0300

    refactor after 19 lesson

--- src/app/appReducer.ts
+++ src/app/appReducer.ts
@@ -3,7 +3,7 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 const appInitialState = {
   status: "idle" as StatusesType,
   error: null as string | null,
-  isInitialized: false,
+  isInitialized: false as boolean,
 };
 
 const slice = createSlice({
